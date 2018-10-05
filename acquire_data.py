677640a233c1222e7bf1f2973eab4a3b93298de4
EC2 Default User <ec2-user@ip-172-31-31-243.ec2.internal>
2018-10-05 00:40:56 +0000

    Network created and hyperparemter information optimized

---
+++ acquire_data.py
@@ -0,0 +1,50 @@
+import numpy as np
+import struct 
+
+def get_data():
+    """
+    Returns tuple (training_data, testing_data) where training_data is a list
+    of tuples (training_image, training_label) where the training_label is
+    the label corresponding to the image. testing_data is similar to
+    training_data. 
+    """
+
+    ftrain_images = "data/train-images-idx3-ubyte"
+    ftrain_labels = "data/train-labels-idx1-ubyte"
+    ftest_images = "data/t10k-images-idx3-ubyte"
+    ftest_labels = "data/t10k-labels-idx1-ubyte"
+
+    with open(ftrain_images, "rb") as train_img:
+        magic_num, train_num, rows, cols = struct.unpack(">iiii", train_img.read(16))
+        training_images = np.fromfile(train_img,
+                dtype=np.uint8).reshape(train_num, rows*cols,1)
+    
+    with open(ftrain_labels, "rb") as train_lbls:
+        magic_num, train_num = struct.unpack(">ii", train_lbls.read(8))
+        training_labels = np.fromfile(train_lbls, dtype=np.int8)
+
+    with open(ftest_images, "rb") as test_img:
+        magic_num, test_num, rows, cols = struct.unpack(">iiii", test_img.read(16))
+        testing_images = np.fromfile(test_img,
+                dtype=np.uint8).reshape(test_num, rows*cols,1)
+        
+    with open(ftest_labels, "rb") as test_lbls:
+        magic_num, test_num = struct.unpack(">ii", test_lbls.read(8))
+        testing_labels = np.fromfile(test_lbls, dtype=np.int8)
+
+    pixel_normalization = 255
+    training_images = training_images.astype(np.float32)
+    training_images = training_images/pixel_normalization
+    training_labels = [vectorize(training_labels[x]) for x in xrange(len(training_labels))]
+    testing_images = testing_images.astype(np.float32)
+    testing_images = testing_images/pixel_normalization
+    testing_labels = [vectorize(testing_labels[x]) for x in xrange(len(testing_labels))]
+
+    training_data = zip(training_images, training_labels)
+    testing_data = zip(testing_images, testing_labels)
+    return (training_data, testing_data)
+
+def vectorize(y):
+    expected_output = np.zeros([10,1])
+    expected_output[y] = float(1)
+    return expected_output
---
+++ optimize_network.py
@@ -0,0 +1,61 @@
+import numpy as np
+import hyperopt
+from functools import partial
+
+
+# cost function: testing cost
+# epochs: 5,10,20,30,40,50,75,100,200
+# hidden layers 2-3 NN: 0, 10, 20, 30, 40, 50, 75, 100, 200, 300, 500  
+# mini_batch_size: 1, 5, 10, 25, 50, 75, 100, 500, 1000, 5000, 10000
+# learning_rate: 0.0001, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0,
+#               10.0, 25.0, 50.0, 100.0
+# regularization_parameter: 0.0001, 0.001, 0.01, 0.05, 0.1, 0.25. 0.5, 1.0,
+#               2.5, 5.0, 10.0, 25.0, 50.0, 100.0
+
+# only testing for 3 layer NN, but could test for 4 and 5 layers
+
+def log_uniform_int(name, lower, upper):
+    return hyperopt.hp.qloguniform(name, np.log(lower), np.log(upper), q=1)
+
+def round_optimized(x, base=5):
+    return int(base * round(float(x)/base))
+
+parameter_space = {
+        'epochs': hyperopt.hp.uniform('epochs', 10, 100),
+        'hidden_layers': hyperopt.hp.uniform('hidden_layers', 10, 200),
+        'mini_batch_size': log_uniform_int('mini_batch_size', 4, 512),
+        'learning_rate': hyperopt.hp.lognormal('learning_rate', 0, 1.5),
+        'regularization_parameter': hyperopt.hp.lognormal('regularization_parameter', 0, 0.4),
+        'early_stopping_threshold': hyperopt.hp.uniform('early_stopping_threshold', 0.005, 0.25)
+}
+
+import acquire_data
+training_data, testing_data = acquire_data.get_data()
+
+import mnist_network
+
+def create_network(parameters):
+
+    hidden_layers = int(parameters['hidden_layers'])
+    layer_sizes = [784, hidden_layers, 10]
+    
+    network = mnist_network.Network(layer_sizes)
+
+    epochs = int(parameters['epochs'])
+    mini_batch_size = round_optimized(parameters['mini_batch_size'])
+    learning_rate = parameters['learning_rate']
+    regularization_parameter = parameters['regularization_parameter']
+    early_stopping_threshold = parameters['early_stopping_threshold']
+
+    network.model(training_data, epochs, mini_batch_size, learning_rate, regularization_parameter, early_stopping_threshold) 
+    cost, accuracy = network.predict(testing_data, regularization_parameter)
+
+    return cost
+
+trials = hyperopt.Trials()
+tpe = partial(hyperopt.tpe.suggest, n_EI_candidates=1000, gamma=0.25, n_startup_jobs=15)
+
+hyperopt.fmin(create_network, trials=trials, space=parameter_space, algo=tpe, max_evals=100)
+
+print trials.best_trial
+
